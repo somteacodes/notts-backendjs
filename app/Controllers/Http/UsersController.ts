7b032be66e7781114686f1b93218299f9898a371
somteacodes <somteacodes@gmail.com>
2022-11-08 13:10:09 +0100

    user avatar upload validate

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,7 +1,7 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 import Application from '@ioc:Adonis/Core/Application';
 import Drive from '@ioc:Adonis/Core/Drive';
- 
+
 export default class UsersController {
   public async index() {
     return 'Get Users';
@@ -9,18 +9,21 @@ export default class UsersController {
 
   public async uploadImage({ request, response }) {
     // TODO get username from auth access token
-    const userImage = request.file('user_image');
+    const userImage = request.file('user_image', {
+      size: '10mb',
+      extnames: ['jpg', 'png', 'gif'],
+    });
+    if (!userImage.isValid) {
+      response.badRequest(userImage.errors[0].message);
+      return;
+    }
 
     try {
-      await userImage.moveToDisk('./', {  
-      }, 's3');
+      await userImage.moveToDisk('./', {}, 's3');
       const url = await Drive.getUrl(userImage.fileName);
       response.ok({ url });
     } catch (error) {
-      response.badRequest({error});
+      response.badRequest({ error });
     }
-    
   }
-
-   
 }
