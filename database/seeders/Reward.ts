42528ab3c5999cb0f8b0d62defa2edf172c2debd
somteacodes <somteacodes@gmail.com>
2023-03-25 14:01:06 +0000

    code cleanup

--- database/seeders/Reward.ts
+++
@@ -1,9 +0,0 @@
-import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
-import RewardFactory from 'Database/factories/RewardFactory'
-
-export default class extends BaseSeeder {
-  public async run () {
-    // Write your database queries inside the run method
-    // await RewardFactory.createMany(20)
-  }
-}
